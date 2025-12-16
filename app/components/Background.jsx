8a96f38998621e96ced92387b6f134c173fa9c3c
yuvraj448-debug <sonu19oct@gmail.com>
2025-12-16 18:28:19 +0530

    Update Background.jsx

--- app/components/Background.jsx
+++ app/components/Background.jsx
@@ -10,35 +10,75 @@ export default function Background() {
       >
         <defs>
           <filter id="glow">
-            <feGaussianBlur stdDeviation="6" result="coloredBlur" />
+            <feGaussianBlur stdDeviation="6" result="blur" />
             <feMerge>
-              <feMergeNode in="coloredBlur" />
+              <feMergeNode in="blur" />
               <feMergeNode in="SourceGraphic" />
             </feMerge>
           </filter>
         </defs>
 
+        {/* Ring 1 */}
         <circle
           cx="200"
           cy="200"
           r="120"
-          className="ring ring1"
+          fill="none"
+          stroke="#4cc9f0"
+          strokeWidth="1.5"
           filter="url(#glow)"
-        />
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            from="0 200 200"
+            to="360 200 200"
+            dur="40s"
+            repeatCount="indefinite"
+          />
+        </circle>
+
+        {/* Ring 2 */}
         <circle
           cx="200"
           cy="200"
           r="150"
-          className="ring ring2"
+          fill="none"
+          stroke="#4cc9f0"
+          strokeWidth="1.5"
           filter="url(#glow)"
-        />
+          opacity="0.8"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            from="360 200 200"
+            to="0 200 200"
+            dur="65s"
+            repeatCount="indefinite"
+          />
+        </circle>
+
+        {/* Ring 3 */}
         <circle
           cx="200"
           cy="200"
           r="180"
-          className="ring ring3"
+          fill="none"
+          stroke="#4cc9f0"
+          strokeWidth="1.2"
           filter="url(#glow)"
-        />
+          opacity="0.6"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            from="0 200 200"
+            to="360 200 200"
+            dur="90s"
+            repeatCount="indefinite"
+          />
+        </circle>
       </svg>
     </div>
   );
