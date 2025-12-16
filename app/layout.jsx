ac3e3aa48916b82da0b4e5b44d2d0b6639c345bd
yuvraj448-debug <sonu19oct@gmail.com>
2025-12-16 21:29:01 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,9 @@
 import "./globals.css";
-import Background from "./components/Background";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white overflow-y-auto">
-        <Background />
+      <body className="bg-black text-white">
         {children}
       </body>
     </html>
