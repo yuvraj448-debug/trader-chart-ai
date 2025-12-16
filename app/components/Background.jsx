3a973422bcbb4c755dce0b23160c556d134697b1
yuvraj448-debug <sonu19oct@gmail.com>
2025-12-16 18:53:07 +0530

    Update Background.jsx

--- app/components/Background.jsx
+++ app/components/Background.jsx
@@ -6,11 +6,9 @@ export default function Background() {
       <div className="bg-stars" />
 
       <div className="bg-rings">
-        <svg viewBox="0 0 500 500">
-          <circle cx="250" cy="250" r="180" />
-          <circle cx="250" cy="250" r="220" />
-          <circle cx="250" cy="250" r="260" />
-        </svg>
+        <div className="ring one"></div>
+        <div className="ring two"></div>
+        <div className="ring three"></div>
       </div>
     </div>
   );
