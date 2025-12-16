6d07b1a5039da22bfc52f4ea50b8b87adc3f3b6c
yuvraj448-debug <sonu19oct@gmail.com>
2025-12-16 18:44:03 +0530

    Update Background.jsx

--- app/components/Background.jsx
+++ app/components/Background.jsx
@@ -3,19 +3,21 @@
 export default function Background() {
   return (
     <div className="bg-wrapper">
-      {/* Stars */}
       <div className="stars" />
 
-      {/* Rotating Rings */}
-      <svg
-        className="rings"
-        viewBox="0 0 600 600"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle cx="300" cy="300" r="170" />
-        <circle cx="300" cy="300" r="220" />
-        <circle cx="300" cy="300" r="270" />
-      </svg>
+      {/* Ring container (POSITION ONLY) */}
+      <div className="ring-holder">
+        {/* SVG rotates INSIDE */}
+        <svg
+          className="rings"
+          viewBox="0 0 600 600"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="300" cy="300" r="170" />
+          <circle cx="300" cy="300" r="220" />
+          <circle cx="300" cy="300" r="270" />
+        </svg>
+      </div>
     </div>
   );
 }
