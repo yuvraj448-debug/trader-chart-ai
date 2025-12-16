6f9f24457c255815f7411f07ad17ff37f11be3d8
yuvraj448-debug <sonu19oct@gmail.com>
2025-12-16 21:25:05 +0530

    Update Background.jsx

--- app/components/Background.jsx
+++ app/components/Background.jsx
@@ -1,22 +1,14 @@
 export default function Background() {
   return (
     <div
-      className="
-        fixed inset-0 
-        -z-10 
-        pointer-events-none
-        overflow-hidden
-      "
+      className="fixed inset-0 -z-10 overflow-hidden pointer-events-none"
+      aria-hidden="true"
     >
-      {/* Stars */}
-      <div className="absolute inset-0 bg-[radial-gradient(white_1px,transparent_1px)] bg-[size:24px_24px] opacity-[0.15]" />
+      <div className="absolute inset-0 bg-black" />
 
-      {/* Animated Rings */}
-      <div className="absolute left-1/2 top-1/2 h-[900px] w-[900px] -translate-x-1/2 -translate-y-1/2 animate-spin-slow">
-        <div className="absolute inset-0 rounded-full border border-cyan-400/30 blur-sm" />
-        <div className="absolute inset-10 rounded-full border border-cyan-400/20" />
-        <div className="absolute inset-20 rounded-full border border-cyan-400/10" />
-      </div>
+      {/* Rings */}
+      <div className="absolute left-1/2 top-1/2 h-[600px] w-[600px] -translate-x-1/2 -translate-y-1/2 rounded-full border border-cyan-400/40 animate-spin-slow" />
+      <div className="absolute left-1/2 top-1/2 h-[800px] w-[800px] -translate-x-1/2 -translate-y-1/2 rounded-full border border-cyan-400/20 animate-spin-reverse" />
     </div>
   );
 }
